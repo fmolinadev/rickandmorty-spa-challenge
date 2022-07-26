362998da6c5cd6f04067ae4b9c7324431082926d
Francisco Molina <fdm.molina@gmail.com>
2022-07-26 16:43:30 -0300

    Change the redirect btn

--- src/components/ErrorAlert.jsx
+++ src/components/ErrorAlert.jsx
@@ -1,12 +1,9 @@
 import React from "react";
-import { Link } from "react-router-dom";
 const ErrorAlert = () => {
   return (
     <div>
       <h1>As usual, something got out of hand...</h1>
-      <button>
-        <Link to="/home">Return</Link>
-      </button>
+      <button onclick="location.reload()">Refres</button>
     </div>
   );
 };
