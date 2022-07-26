ab0f263d8c7f8a5d5471eebc4962781ce7abd728
Francisco Molina <fdm.molina@gmail.com>
2022-07-26 01:35:19 -0300

    Update Body component

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,7 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
+import { useQuery } from "@apollo/client";
+import GET_CHARACTER from "../util/GET_CHARACTER";
+import generateRandomID from "../util/generateRandomID";
+import Character from "./Character";
+import History from "./History";
 
 const Body = () => {
-  return <div>Body</div>;
+  const [open, setOpen] = useState(false);
+  const [randomID, setID] = useState(generateRandomID());
+  // console.log("ID FLAG-->", randomID);
+  const { data, loading } = useQuery(GET_CHARACTER, {
+    variables: {
+      id: randomID,
+    },
+  });
+  // console.log("INFO FLAG-->", data);
+  const [resultRequest, setResult] = useState(null);
+  const [history, setHistory] = useState([]);
+  //On click:
+  const handleButton = () => {
+    setID(generateRandomID());
+    setResult(data.character);
+    setHistory([data.character, ...history]);
+  };
+
+  return (
+    <div>
+      <Character
+        infoCharacter={resultRequest}
+        runInfo={handleButton}
+        loader={loading}
+      />
+      <div>
+        <div onClick={() => setOpen(!open)} aria-label="menu">
+          <i className="fi fi-br-ballot"></i>
+          <h5>History</h5>
+        </div>
+        {open && (
+          <History
+            history={history}
+            open={open}
+            setOpen={setOpen}
+            loader={loading}
+          />
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Body;
