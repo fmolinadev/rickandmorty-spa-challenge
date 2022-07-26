8af096f67f98b3049f33893c207f0350e8aa32b4
Francisco Molina <fdm.molina@gmail.com>
2022-07-26 01:35:50 -0300

    Update the App file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import LandingPage from "./pages/LandingPage";
 import ErrorPage from "./pages/ErrorPage.jsx";
 import Header from "./components/Header/Index.jsx";
+import Footer from "./components/Footer";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage.jsx";
+// import HistoryPage from "./pages/HistoryPage";
 
 function App() {
   return (
@@ -11,8 +13,10 @@ function App() {
       <Routes>
         <Route exact path="/" element={<LandingPage />} />
         <Route exact path="/home" element={<HomePage />} />
+        {/* <Route exact path="/history" element={<HistoryPage />} /> */}
         <Route path="*" element={<ErrorPage />} />
       </Routes>
+      <Footer />
     </>
   );
 }
