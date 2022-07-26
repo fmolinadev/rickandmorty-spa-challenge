ef9ef7ca4631c6735185a9d6d84ad7841c62e68b
Francisco Molina <fdm.molina@gmail.com>
2022-07-26 15:07:16 -0300

    Add responsive and styles

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,37 +1,47 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logo from "../assets/rick-and-morty-name-png.png";
 import rickandmorty from "../assets/rick-and-morty-characters-png.png";
+import {
+  LandingContainer,
+  ContainerLeft,
+  ContainerRight,
+  ImageContainer,
+  TextContainer,
+  ButtonContainer,
+  TextHeader,
+  TextDescription,
+} from "../styles/LandingStyles";
+import { Button } from "../styles/Button";
 
 const Landing = () => {
   const noDecoration = { textDecoration: "none", color: "inherit" };
   return (
-    <div>
-      <div>
-        <img
+    <LandingContainer>
+      <ContainerLeft>
+        <ImageContainer
           src={rickandmorty}
           alt="characters"
           aria-label="characters image"
         />
-        <img src={logo} alt="logo" aria-label="logo" />
-      </div>
-      <div>
-        <div>
-          <h1>See what's happening here</h1>
-          <h3>
+      </ContainerLeft>
+      <ContainerRight>
+        <TextContainer>
+          <TextHeader>See what's happening here</TextHeader>
+          <br />
+          <TextDescription>
             All information about your favorite Rick and Morty characters, out
             of control (well... this wouldn't be news)!
-          </h3>
-        </div>
-        <button>
-          {
+          </TextDescription>
+        </TextContainer>
+        <ButtonContainer>
+          <Button>
             <Link to="/home" style={noDecoration}>
               See now
             </Link>
-          }
-        </button>
-      </div>
-    </div>
+          </Button>
+        </ButtonContainer>
+      </ContainerRight>
+    </LandingContainer>
   );
 };
 
