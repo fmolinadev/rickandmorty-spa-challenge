168cd14dfef3eb3984736bdbe23ca5f5e1ce593d
Francisco Molina <fdm.molina@gmail.com>
2022-07-26 19:03:16 -0300

    Create the Character Styles

---
+++ src/styles/CharacterStyles.js
@@ -0,0 +1,76 @@
+import styled from "styled-components";
+
+export const CharacterContainer = styled.section``;
+
+export const DefaultSetting = styled.div`
+  display: block;
+  position: relative;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  margin: 0 auto;
+  text-align: center;
+  padding-left: 25%;
+  padding-top: 19%;
+  @media (max-width: 720px) {
+    padding-left: 4%;
+    padding-top: 65%;
+  }
+`;
+
+export const DefaulText = styled.h1`
+  font-size: 22px;
+  padding-bottom: 10px;
+`;
+
+export const LoaderContainer = styled.div`
+  display: flex;
+  padding-left: 65%;
+  padding-top: 19%;
+  @media (max-width: 720px) {
+    padding-left: 54%;
+    padding-top: 65%;
+  }
+`;
+
+export const CharacterCard = styled.article`
+  display: grid;
+  grid-template-columns: 1fr 2fr;
+  grid-column-gap: 2em;
+  grid-row-gap: 2em;
+  @media (max-width: 720px) {
+    grid-template-columns: 1fr;
+    grid-row-gap: 4em;
+    grid-column-gap: 3em;
+  }
+`;
+
+export const CharacterLeft = styled.div``;
+
+export const CharacterImage = styled.img`
+  border: 2px;
+  border-radius: 6px;
+`;
+
+export const CharacterData = styled.div`
+  width: 85%;
+`;
+
+export const CharacterHeader = styled.div`
+  height: 45px;
+`;
+
+export const CharacterName = styled.h1`
+  font-size: 23px;
+`;
+export const CharacterTag = styled.h6`
+  font-size: 14px;
+`;
+
+export const CharacterResults = styled.div`
+  padding-top: 28px;
+`;
+
+export const DataContainer = styled.div`
+  padding: 6px 0px;
+`;
