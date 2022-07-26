f7b24ddff435d364e157871e1ca99cbbdc3e14c1
Francisco Molina <fdm.molina@gmail.com>
2022-07-26 20:29:11 -0300

    Fix details

--- src/styles/Body.js
+++ src/styles/Body.js
@@ -2,21 +2,37 @@ import styled from "styled-components";
 
 export const BodyContainer = styled.div`
   display: grid;
-  padding-top: 1%;
+  padding-top: 6%;
   margin: 0px 16px;
   grid-template-columns: 4fr 2fr;
-  grid-column-gap: 2em;
-  grid-row-gap: 2em;
+  grid-column-gap: 1em;
+  grid-row-gap: 1em;
   @media (max-width: 720px) {
     grid-template-columns: 1fr;
-    grid-row-gap: 4em;
-    grid-column-gap: 3em;
+    grid-row-gap: 3em;
+    grid-column-gap: 2em;
   }
 `;
 
 export const CharacterContainer = styled.div``;
 
+export const HistoryAside = styled.aside`
+  margin: 0px 16px;
+  @media (max-width: 720px) {
+    margin: 0px 2px;
+  }
+`;
+
 export const ToggleMenu = styled.div`
   display: flex;
   flex-direction: colum;
+  justify-content: flex-end;
+  align-item: center;
+  padding-bottom: 7px;
+  & i {
+    padding-right: 5px;
+  }
+  @media (max-width: 720px) {
+    justify-content: flex-start;
+  }
 `;
