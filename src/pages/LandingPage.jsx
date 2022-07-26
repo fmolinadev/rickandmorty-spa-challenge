fece46601461ae17342323a31e40779cf0db99c1
Francisco Molina <fdm.molina@gmail.com>
2022-07-25 21:38:21 -0300

    Update Landing Page

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../assets/rick-and-morty-name-png.png";
+import rickandmorty from "../assets/rick-and-morty-characters-png.png";
 
 const Landing = () => {
-  return <div>R&M SPA</div>;
+  const noDecoration = { textDecoration: "none", color: "inherit" };
+  return (
+    <div>
+      <div>
+        <img
+          src={rickandmorty}
+          alt="characters"
+          aria-label="characters image"
+        />
+        <img src={logo} alt="logo" aria-label="logo" />
+      </div>
+      <div>
+        <div>
+          <h1>See what's happening here</h1>
+          <h3>
+            All information about your favorite Rick and Morty characters, out
+            of control (well... this wouldn't be news)!
+          </h3>
+        </div>
+        <button>
+          {
+            <Link to="/home" style={noDecoration}>
+              See now
+            </Link>
+          }
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Landing;
