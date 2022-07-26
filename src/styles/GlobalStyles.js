43c6a4aa2ddb2988df21d2c5b2727c8bd35e3fb9
Francisco Molina <fdm.molina@gmail.com>
2022-07-26 19:01:56 -0300

    Set hr opacity

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -7,12 +7,15 @@ const GlobalStyle = createGlobalStyle`
     font-family: 'Play', sans-serif;
     font-size: 16px;
     color: #303030;
-    
   }
 
   body {
     max-width: 1400px;
     margin: auto;
+  }
+
+  hr {
+    opacity: 0.4;
   }
 `;
 
