9ce8da150c2a4eb9941f29a293155ef0f0507a6c
Francisco Molina <fdm.molina@gmail.com>
2022-07-26 03:12:06 -0300

    Update History page

--- src/pages/HistoryPage.jsx
+++ src/pages/HistoryPage.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import History from "../components/History";
+
 const HistoryPage = () => {
   return (
     <div>
       <h2>Your characters generated:</h2>
-      <History />
     </div>
   );
 };
