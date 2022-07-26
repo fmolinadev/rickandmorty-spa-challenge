b2e0812b0178dd1622c7562baae4988b1b8f4ccb
Francisco Molina <fdm.molina@gmail.com>
2022-07-26 15:08:44 -0300

    Update component and add responsive and styles

--- src/components/Header/NavMobile.jsx
+++ src/components/Header/NavMobile.jsx
@@ -1,26 +1,30 @@
 import { Link } from "react-router-dom";
+import {
+  NavbarMobile,
+  ToggleIcon,
+  NavbarOptions,
+  Option,
+} from "../../styles/NavbarMobile";
 const Mobile = ({ open, setOpen }) => {
   const noDecoration = { textDecoration: "none", color: "inherit" };
   return (
-    <div>
-      <div onClick={() => setOpen(!open)}>
+    <NavbarMobile>
+      <ToggleIcon onClick={() => setOpen(!open)}>
         <i className="fi fi-br-cross"></i>
-      </div>
-      <div onClick={() => setOpen(!open)}>
-        <ul aria-label="options">
-          <li>
-            <Link to="/home" style={noDecoration}>
-              <span>Home</span>
-            </Link>
-          </li>
-          <li>
-            {/* <Link to="/history" style={noDecoration}> */}
-            <span>History</span>
-            {/* </Link> */}
-          </li>
-        </ul>
-      </div>
-    </div>
+      </ToggleIcon>
+      <NavbarOptions aria-label="options" onClick={() => setOpen(!open)}>
+        <Option>
+          <Link to="/home" style={noDecoration}>
+            <span>Home</span>
+          </Link>
+        </Option>
+        <Option>
+          {/* <Link to="/history" style={noDecoration}> */}
+          <span>History</span>
+          {/* </Link> */}
+        </Option>
+      </NavbarOptions>
+    </NavbarMobile>
   );
 };
 
