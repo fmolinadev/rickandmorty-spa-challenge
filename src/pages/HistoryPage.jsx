9bc299c2216d984fdf86c34bdfd28693b27ab9b1
Francisco Molina <fdm.molina@gmail.com>
2022-07-26 01:35:36 -0300

    Add a History Page

---
+++ src/pages/HistoryPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import History from "../components/History";
+const HistoryPage = () => {
+  return (
+    <div>
+      <h2>Your characters generated:</h2>
+      <History />
+    </div>
+  );
+};
+
+export default HistoryPage;
