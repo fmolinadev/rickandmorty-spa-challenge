a1e80ac0914451b8346c333e5e6383df11f21708
Francisco Molina <fdm.molina@gmail.com>
2022-07-26 19:02:23 -0300

    Update character styles

--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -3,87 +3,107 @@ import Placeholder from "../assets/placeholder-img.png";
 import Loader from "./Loader";
 import { Button } from "../styles/Button";
 import ErrorAlert from "./ErrorAlert";
+import {
+  CharacterContainer,
+  DefaultSetting,
+  DefaulText,
+  LoaderContainer,
+  CharacterCard,
+  CharacterImage,
+  CharacterHeader,
+  CharacterData,
+  CharacterLeft,
+  CharacterName,
+  CharacterTag,
+  CharacterResults,
+  DataContainer,
+} from "../styles/CharacterStyles.js";
 
 const Character = ({ infoCharacter, runInfo, loader, error }) => {
   if (error) return <ErrorAlert />;
-
   return (
-    <section>
+    <CharacterContainer>
       {!infoCharacter ? (
-        <>
-          <h1>Click on the button to begin!</h1>
+        <DefaultSetting>
+          <DefaulText>Click on the button to begin!</DefaulText>
           <Button onClick={() => runInfo()}>See a character</Button>
-        </>
+        </DefaultSetting>
       ) : loader ? (
-        <Loader />
+        <LoaderContainer>
+          <Loader />
+        </LoaderContainer>
       ) : (
-        <>
-          <div>
+        <CharacterCard>
+          <CharacterLeft>
+            <CharacterImage
+              src={infoCharacter.image ? infoCharacter.image : Placeholder}
+              alt="character"
+              aria-label="image of a character"
+            />
             <div>
-              <img
-                src={infoCharacter.image ? infoCharacter.image : Placeholder}
-                alt="character"
-                aria-label="image of a character"
-              ></img>
+              <Button onClick={() => runInfo()}>Get new character</Button>
             </div>
-            <div>
-              <div>
-                <h1>{infoCharacter.name ? infoCharacter.name : "Not known"}</h1>
-                <h6>id: {infoCharacter.id ? infoCharacter.id : "Not known"}</h6>
-              </div>
-              <div>
-                <div>
-                  <span>Status: </span>
-                  {infoCharacter.status ? infoCharacter.status : "Not known"}
-                </div>
-                <hr />
-                <div>
-                  <span>Species: </span>
-                  {infoCharacter.species ? infoCharacter.species : "Not known"}
-                </div>
-                <hr />
-                <div>
-                  <span>Type: </span>
-                  {infoCharacter.type ? infoCharacter.type : "Not known"}
-                </div>
-                <hr />
-                <div>
-                  <span>Gender: </span>
-                  {infoCharacter.gender ? infoCharacter.gender : "Not known"}
-                </div>
-                <hr />
-                <div>
-                  <span>Origin: </span>
-                  {infoCharacter.origin.name
-                    ? infoCharacter.origin.name
-                    : "Not known"}
-                </div>
-                <hr />
-                <div>
-                  <span>Location: </span>
-                  {infoCharacter.location.name
-                    ? infoCharacter.location.name
-                    : "Not known"}
-                </div>
-                <hr />
-                <div>
-                  <span>Created at: </span>
-                  {new Date(
-                    infoCharacter.created ? infoCharacter.created : "Not known"
-                  )
-                    .toString()
-                    .split(" ")
-                    .slice(0, 4)
-                    .join(" ")}
-                </div>
-                <hr />
-              </div>
-            </div>
-          </div>
-          <Button onClick={() => runInfo()}>Generate a new character</Button>
-        </>
+          </CharacterLeft>
+          <CharacterData>
+            <CharacterHeader>
+              <CharacterName>
+                {infoCharacter.name ? infoCharacter.name : "Not known"}
+              </CharacterName>
+              <CharacterTag>
+                id: #{infoCharacter.id ? infoCharacter.id : "Not known"}
+              </CharacterTag>
+            </CharacterHeader>
+            <CharacterResults>
+              <DataContainer>
+                <span>Status: </span>
+                {infoCharacter.status ? infoCharacter.status : "Not known"}
+              </DataContainer>
+              <hr />
+              <DataContainer>
+                <span>Species: </span>
+                {infoCharacter.species ? infoCharacter.species : "Not known"}
+              </DataContainer>
+              <hr />
+              <DataContainer>
+                <span>Type: </span>
+                {infoCharacter.type ? infoCharacter.type : "Not known"}
+              </DataContainer>
+              <hr />
+              <DataContainer>
+                <span>Gender: </span>
+                {infoCharacter.gender ? infoCharacter.gender : "Not known"}
+              </DataContainer>
+              <hr />
+              <DataContainer>
+                <span>Origin: </span>
+                {infoCharacter.origin.name
+                  ? infoCharacter.origin.name
+                  : "Not known"}
+              </DataContainer>
+              <hr />
+              <DataContainer>
+                <span>Location: </span>
+                {infoCharacter.location.name
+                  ? infoCharacter.location.name
+                  : "Not known"}
+              </DataContainer>
+              <hr />
+              <DataContainer>
+                <span>Created at: </span>
+                {new Date(
+                  infoCharacter.created ? infoCharacter.created : "Not known"
+                )
+                  .toString()
+                  .split(" ")
+                  .slice(0, 4)
+                  .join(" ")}
+              </DataContainer>
+              <hr />
+            </CharacterResults>
+          </CharacterData>
+        </CharacterCard>
       )}
-    </section>
+    </CharacterContainer>
   );
 };
 
