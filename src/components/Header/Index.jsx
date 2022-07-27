df0caad0b5f18b7dae7f6974f939d10505c12e42
Francisco Molina <fdm.molina@gmail.com>
2022-07-26 21:24:36 -0300

    Reload page if actual url is "home" and click in logo

--- src/components/Header/Index.jsx
+++ src/components/Header/Index.jsx
@@ -14,12 +14,28 @@ import {
 const Header = () => {
   const [open, setOpen] = useState(false);
   const noDecoration = { textDecoration: "none", color: "inherit" };
+  const actualUrl = window.location.pathname;
+
+  const reloadPage = (e) => {
+    window.location.reload();
+  };
 
   return (
     <HeaderContainer>
-      <Link to="/home" style={noDecoration}>
-        <BrandSite src={logo} alt="logo" aria-label="logo spa" />
-      </Link>
+      {actualUrl[1] === "h" && actualUrl[4] === "e" ? (
+        <span
+          onClick={(e) => {
+            reloadPage();
+          }}
+          style={noDecoration}
+        >
+          <BrandSite src={logo} alt="logo" aria-label="logo spa" />
+        </span>
+      ) : (
+        <Link to="/home" style={noDecoration}>
+          <BrandSite src={logo} alt="logo" aria-label="logo spa" />
+        </Link>
+      )}
       <MenuContainer>
         <MenuContainerWeb aria-label="menu">
           <Web />
