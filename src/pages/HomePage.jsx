ee7e9d5a8ad76e28f6c50bc21676666c67652ba2
Francisco Molina <fdm.molina@gmail.com>
2022-07-26 23:07:53 -0300

    Updates

--- src/pages/HomePage.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import Body from "../components/Body";
-const HomePage = () => {
-  return <Body />;
-};
-
-export default HomePage;
--- src/util/generateRandomID.js
+++
@@ -1,6 +0,0 @@
-const generateRandomId = () => {
-  const random = Math.floor(Math.random() * 492 + 4);
-  return random;
-};
-
-export default generateRandomId;
